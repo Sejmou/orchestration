8e21f77fa1838d44c3476dc9231df1f38f398014
Sejmou <samo.kolter@gmail.com>
2025-06-26 01:45:31 +0200

    fix ch_update_spotify_views

--- flows/ch_update_spotify_views.py
+++ flows/ch_update_spotify_views.py
@@ -11,8 +11,6 @@ def update_spotify_views():
     """
     Flow to update Spotify views in ClickHouse.
     """
-    etl_creds = ClickHouseCredentials.load("clickhouse-etl-config")
-    client = create_client(etl_creds)
     views = [
         "spotify.top_track_ids_by_streams_de_mv",
         "spotify.top_track_ids_by_streams_at_mv",
@@ -27,18 +25,21 @@ def update_spotify_views():
 
 
 @task(log_prints=True)
-def update_refreshable_materialized_view(client: ClickHouseClient, view_name: str):
+def update_refreshable_materialized_view(view_name: str):
+    etl_creds = ClickHouseCredentials.load("clickhouse-etl-config")
+    client = create_client(etl_creds)
     print(f"Refreshing materialized view: {view_name}")
-    client.command(f"REFRESH MATERIALIZED VIEW {view_name}")
+    client.command(f"SYSTEM REFRESH VIEW {view_name}")
     print(f"Waiting for view {view_name} to be refreshed...")
     client.command(f"SYSTEM WAIT VIEW {view_name}")
     print(f"View {view_name} refreshed successfully.")
 
 
 if __name__ == "__main__":
-    update_spotify_views.deploy(
-        name="Update Spotify Views",
-        work_pool_name="Docker",
-        image="my_registry/my_image:my_image_tag",
-        push=False,
-    )
+    update_spotify_views.serve()
+    # update_spotify_views.deploy(
+    #     name="Update Spotify Views",
+    #     work_pool_name="Docker",
+    #     image="my_registry/my_image:my_image_tag",
+    #     push=False,
+    # )
